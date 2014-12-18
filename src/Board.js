fc88089b938fe575886f86223493c7e136ee3661
jasonyangftd <jasonyangftd@gmail.com>
2014-12-17 16:25:35 -0800

    Solved Rooks problem 1

--- src/Board.js
+++ src/Board.js
@@ -79,12 +79,23 @@
     //
     // test if a specific row on this board contains a conflict
     hasRowConflictAt: function(rowIndex) {
-      return false; // fixme
+      var counter = 0;
+      var currentArray = this.get(rowIndex);
+      for (var i = 0; i < currentArray.length; i++) {
+        counter+= currentArray[i];
+      }
+      return counter > 1;
     },
 
     // test if any rows on this board contain conflicts
     hasAnyRowConflicts: function() {
-      return false; // fixme
+      var size = this.get('n');
+      for (var i = 0; i < size; i++) {
+        if (this.hasRowConflictAt(i)) {
+          return true;
+        }
+      }
+      return false;
     },
 
 
@@ -94,12 +105,24 @@
     //
     // test if a specific column on this board contains a conflict
     hasColConflictAt: function(colIndex) {
-      return false; // fixme
+      var size = this.get('n');
+      var counter = 0;
+      for (var i = 0; i < size; i++) {
+        var currentArray = this.get(i);
+        counter+= currentArray[colIndex];
+      }
+      return counter > 1;
     },
 
     // test if any columns on this board contain conflicts
     hasAnyColConflicts: function() {
-      return false; // fixme
+      var size = this.get('n');
+      for (var i = 0; i < size; i++) {
+        if (this.hasColConflictAt(i)) {
+          return true;
+        }
+      }
+      return false;
     },
 
 
@@ -109,12 +132,28 @@
     //
     // test if a specific major diagonal on this board contains a conflict
     hasMajorDiagonalConflictAt: function(majorDiagonalColumnIndexAtFirstRow) {
-      return false; // fixme
+      var size = this.get('n');
+      var counter = 0;
+      var colIndex = majorDiagonalColumnIndexAtFirstRow;
+      for (var i = 0; i < size; i++) {
+        var currentArray = this.get(i);
+        colIndex++;
+          if (colIndex < size) {
+            counter += currentArray[colIndex];
+          }
+      }
+      return counter > 1;
     },
 
     // test if any major diagonals on this board contain conflicts
     hasAnyMajorDiagonalConflicts: function() {
-      return false; // fixme
+      var size = this.get('n');
+      for (var i = 0; i < size; i++) {
+        if (this.hasMajorDiagonalConflictAt(i)) {
+          return true;
+        }
+      }
+      return false;
     },
 
 
@@ -124,12 +163,28 @@
     //
     // test if a specific minor diagonal on this board contains a conflict
     hasMinorDiagonalConflictAt: function(minorDiagonalColumnIndexAtFirstRow) {
-      return false; // fixme
+      var size = this.get('n');
+      var counter = 0;
+      var colIndex = minorDiagonalColumnIndexAtFirstRow;
+      for (var i = 0; i < size; i++) {
+        var currentArray = this.get(i);
+        colIndex--;
+          if (colIndex >= 0) {
+            counter += currentArray[colIndex];
+          }
+      }
+      return counter > 1;
     },
 
     // test if any minor diagonals on this board contain conflicts
     hasAnyMinorDiagonalConflicts: function() {
-      return false; // fixme
+      var size = this.get('n');
+      for (var i = 0; i < size; i++) {
+        if (this.hasMinorDiagonalConflictAt(i)) {
+          return true;
+        }
+      }
+      return false;
     }
 
     /*--------------------  End of Helper Functions  ---------------------*/
--- src/solvers.js
+++ src/solvers.js
@@ -14,10 +14,35 @@
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n rooks placed such that none of them can attack each other
 
 window.findNRooksSolution = function(n) {
-  var solution = undefined; //fixme
+  var solution = [];
+  var board = new Board({n:n});
+  var innerFunction = function(rowIndex){
+    var tempArray = board.get(rowIndex);
+    for (var i = 0; i < n; i++){
+      board.get(rowIndex)[rowIndex] = 1;
 
+      if (!board.hasColConflictAt(i)) {
+        solution.push(board.get(rowIndex));
+        break;
+      } else {
+        board.get(rowIndex)[rowIndex] = 0;
+      }
+    }
+  }
+  // want create a n*n matrix to represent the solution
+  // need to check whether placing the rook at certain location will cause row or column conflicts
+  // if so, we want backtrack and check the next possiable location.
+  // repeat
+  // check if the location where the next rook will be placed will cause conflict
+  // recursion
   console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
-  return solution;
+  // return solution;
+  for (var i = 0; i < n; i++) {
+    innerFunction(i);
+  }
+  if (solution.length === n) {
+      return solution;
+  }
 };
 
 
